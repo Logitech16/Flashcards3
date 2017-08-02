ec0c411fcac7036e44239c00c0caa2c1c1507a73
iD Student <idstudent@dna0f8480.sunet>
2017-08-02 10:23:59 -0700

    commit

--- Flashcards2/ViewController.swift
+++ Flashcards2/ViewController.swift
@@ -1,25 +1,119 @@
+
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+33
+34
+35
+36
+37
+38
+39
+40
+41
+42
+43
+44
+45
+46
+47
+48
+49
+50
+51
+52
+53
+54
+55
+56
+57
+58
+59
 //
 //  ViewController.swift
-//  Flashcards2
+//  Flashcards
 //
-//  Created by iD Student on 8/1/17.
+//  Created by iD Student on 5/5/17.
 //  Copyright © 2017 iD Tech. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIPickerViewDataSource,UIPickerViewDelegate  {
+    
+    @IBOutlet weak var TextView: UITextView!
+    
+    @IBOutlet weak var answerPickerView: UIPickerView!
+    
+    @IBOutlet weak var QuestionLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        answerPickerView.dataSource = self;
+        answerPickerView.delegate = self;
+        
+        setupCardUI()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func setupCardUI() {
+        TextView.text = CardCollection.instance.currentCard.question
+        
+        QuestionLabel.text = "Question \(CardCollection.instance.currentIndex + 1)/\(CardCollection.instance.cards.count)"
+    }
+    
+    // Pickerview Data Source
+    // returns the number of 'columns' to display.
+    public func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1;
+    }
+    
+    
+    // returns the # of rows in each component..
+    public func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return CardCollection.instance.currentCard.options.count
+    }
+    
+    // Pickerview delegates
+    // returns text of option at a given row.
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return CardCollection.instance.currentCard.options[row];
+    }
+    
+    
+    
 }
-
